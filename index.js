855bafe0afa6eea3461cb609dbf587bb4347a81b
Annie <pyravice@gmail.com>
2022-07-30 14:55:53 -0600

    Beefed up Sentry and added CSS changes.

--- index.js
+++ index.js
@@ -18,12 +18,19 @@ const env = process.env.NODE_ENV;
 
 
 
-/** Sentry config */
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({
+      app,
+    }),
+  ],
   tracesSampleRate: 1.0,
 });
-
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+app.use(Sentry.Handlers.errorHandler());
 
 /** Auth0 config */
 const config = {
